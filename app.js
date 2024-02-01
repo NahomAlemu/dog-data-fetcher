96a36d18932a42b0731d53f7c30a9ee5252180e5
NahomAlemu <nahom.worku2015@gmail.com>
2024-01-31 18:20:01 -0800

    code enhancement to app.js

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const port = 3001;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.send(`
+      <h1>Welcome to the Dog Data Fetcher</h1>
+      <p>Click <a href="/dog-breeds.html">here</a> to view dog breeds information.</p>
+  `);
 });
+
+app.use((req, res) => {
+  res.status(404).send('404 - Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`Server running successfully at http://localhost:${port}`);
+});
+
